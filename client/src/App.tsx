acd1164cddc048d5347c151ef821619f5254b077
Greg Skriloff <35093316+gskril@users.noreply.github.com>
2025-08-05 03:49:39 -0400

    Show indexer status

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,18 @@
 import { Route, Routes } from 'react-router-dom'
 import { Home } from './screens/Home'
 import { Proposal } from './screens/Proposal'
+import { IndexerStatus } from './components/IndexerStatus'
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/proposal/:id" element={<Proposal />} />
-    </Routes>
+    <>
+      <IndexerStatus />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/proposal/:id" element={<Proposal />} />
+      </Routes>
+    </>
   )
 }
 
---
+++ client/src/components/IndexerStatus.tsx
@@ -0,0 +1,45 @@
+import { useQuery } from '@tanstack/react-query'
+
+const url = new URL('/status', import.meta.env.VITE_PONDER_URL).toString()
+
+export function IndexerStatus() {
+  const { data } = useQuery({
+    queryKey: ['indexer-status'],
+    refetchInterval: 5000,
+    queryFn: async () => {
+      const res = await fetch(url)
+      const json = (await res.json()) as {
+        zkSync: {
+          id: number
+          block: {
+            number: number
+            timestamp: number
+          }
+        }
+      }
+
+      const currentTimestamp = Math.floor(Date.now() / 1000)
+
+      // Check if the current timestamp is within 30 seconds of the block timestamp
+      const isReady = currentTimestamp - json.zkSync.block.timestamp < 30
+
+      return { isReady, status: json }
+    },
+  })
+
+  const { isReady, status } = data ?? {}
+
+  if (status && !isReady) {
+    return (
+      <div className="w-full bg-primary-brand/5 p-2 text-center text-sm">
+        <span>
+          Data is being indexed... Currently at{' '}
+          {new Intl.DateTimeFormat('en-US', {
+            dateStyle: 'medium',
+            timeStyle: 'medium',
+          }).format(new Date(status.zkSync.block.timestamp * 1000))}
+        </span>
+      </div>
+    )
+  }
+}
