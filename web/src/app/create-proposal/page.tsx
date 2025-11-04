a675a793a5234596ef7aacaccf7a6ef47ed9fb82
Greg Skriloff <35093316+gskril@users.noreply.github.com>
2025-11-04 10:03:13 -0500

    Placeholder for create-proposal page

---
+++ web/src/app/create-proposal/page.tsx
@@ -0,0 +1,10 @@
+import { Nav } from '@/components/Nav'
+
+export default function CreateProposal() {
+  return (
+    <div className="container">
+      <Nav />
+      <div>Create Proposal</div>
+    </div>
+  )
+}
