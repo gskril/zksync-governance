f94293f5b5ee9783dd3390e49893c98f4836c562
Greg Skriloff <35093316+gskril@users.noreply.github.com>
2025-12-05 05:09:18 -0500

    Create proposal page working

--- web/src/app/create-proposal/client.tsx
+++ web/src/app/create-proposal/client.tsx
@@ -1,10 +1,10 @@
 'use client'
 
 import { Nav } from '@/components/Nav'
-import { Button } from '@/components/ui/button'
+import { Button, buttonVariants } from '@/components/ui/button'
 import { InputGroup, InputWrapper } from '@/components/ui/input'
 import { Textarea } from '@/components/ui/textarea'
-import { bigintToFormattedString, GOVERNORS } from '@/lib/utils'
+import { bigintToFormattedString, cn, GOVERNORS } from '@/lib/utils'
 import {
   Select,
   SelectContent,
@@ -15,8 +15,9 @@ import {
 } from '@/components/ui/select'
 import { ZkProtocolGovernor, ZkToken, ZkTokenGovernor } from 'indexer/contracts'
 import { InfoIcon, MinusIcon, PlusIcon } from 'lucide-react'
+import Link from 'next/link'
 import { useEffect, useState } from 'react'
-import { Address, Hex, isAddress, isHex } from 'viem'
+import { Address, decodeEventLog, Hex, isAddress, isHex } from 'viem'
 import {
   useAccount,
   useReadContract,
@@ -39,16 +40,21 @@ export function CreateProposalClient() {
   const tx = useWriteContract()
   const receipt = useWaitForTransactionReceipt({ hash: tx.data })
 
-  const { data: votingPower } = useReadContract({
+  const { data: threshold } = useReadContract({
+    ...ZkTokenGovernor,
+    functionName: 'proposalThreshold',
+    chainId: zksync.id,
+  })
+
+  const { data: votingPower, error: votingPowerError } = useReadContract({
     ...ZkToken,
     functionName: 'getVotes',
     args: address ? [address] : undefined,
     chainId: zksync.id,
   })
 
-  // All governors require 21M votes to create a proposal.
-  const threshold = BigInt(21_000_000) * BigInt(10 ** 18)
-  const hasEnoughVotingPower = (votingPower ?? BigInt(0)) >= threshold
+  const hasEnoughVotingPower =
+    (votingPower ?? BigInt(0)) >= (threshold ?? BigInt(0))
 
   const [transactions, setTransactions] = useState<Transaction[]>([
     { target: '', value: 0, calldata: '' },
@@ -59,7 +65,6 @@ export function CreateProposalClient() {
     const formData = new FormData(e.target as HTMLFormElement)
     const governorContract = formData.get('governorContract') as string
     const proposalMarkdown = formData.get('proposalMarkdown') as string
-    console.log({ governorContract, proposalMarkdown, transactions })
 
     await switchChainAsync({ chainId: zksync.id })
     tx.writeContract({
@@ -81,107 +86,166 @@ export function CreateProposalClient() {
 
       <main className="max-w-2xl mx-auto">
         <h1 className="text-2xl font-bold text-center mb-4">
-          Create a Proposal
+          {receipt.isSuccess ? 'Proposal Created' : 'Create a Proposal'}
         </h1>
 
-        {(!isConnected || !hasEnoughVotingPower) && (
-          <Alert variant="info" className="mb-4">
-            <InfoIcon />
-            <AlertTitle>View-only mode.</AlertTitle>
-            <AlertDescription>
-              Connect a wallet with &gt;21M votes to create a proposal.{' '}
-              {votingPower !== undefined &&
-                `You currently have ${bigintToFormattedString(votingPower)} votes.`}
-            </AlertDescription>
-          </Alert>
-        )}
+        {(() => {
+          if (receipt.isSuccess) {
+            const proposalLogs = receipt.data!.logs
+            const proposalCreatedTopic =
+              '0x7d84a6263ae0d98d3329bd7b46bb4e8d6f98cd35a7adb45c274c8b7fd5ebd5e0'
+            const proposalCreatedLog = proposalLogs.find(
+              (log) => log.topics[0] === proposalCreatedTopic
+            )
+            const decodedLog = proposalCreatedLog
+              ? decodeEventLog({
+                  abi: [ZkProtocolGovernor.abi[10]],
+                  data: proposalCreatedLog.data,
+                  topics: proposalCreatedLog.topics,
+                })
+              : undefined
 
-        <form className="space-y-4" onSubmit={handleCreateProposal}>
-          <InputWrapper label="Governor Contract">
-            <Select
-              name="governorContract"
-              defaultValue={ZkTokenGovernor.address}
-            >
-              <SelectTrigger className="w-full">
-                <SelectValue placeholder="Select a category" />
-              </SelectTrigger>
-              <SelectContent>
-                <SelectGroup>
-                  {Array.from(GOVERNORS.entries()).map(([address, name]) => (
-                    <SelectItem key={address} value={address}>
-                      {name}
-                    </SelectItem>
-                  ))}
-                </SelectGroup>
-              </SelectContent>
-            </Select>
-          </InputWrapper>
+            const proposalId = decodedLog?.args.proposalId
 
-          <InputWrapper label="Proposal Markdown">
-            <Textarea placeholder="" required name="proposalMarkdown" />
-          </InputWrapper>
+            return (
+              <>
+                <p className="text-center">
+                  Proposal is now live onchain. View the proposal or visit
+                  blockchain explorer.
+                </p>
 
-          <div className="flex items-center gap-4 justify-between pt-4">
-            <span>Transactions</span>
-            <Button
-              type="button"
-              variant="outline"
-              size="icon"
-              className="h-8 w-8"
-              onClick={() =>
-                setTransactions([
-                  ...transactions,
-                  { target: '', value: 0, calldata: '' },
-                ])
-              }
-            >
-              <PlusIcon />
-            </Button>
-          </div>
+                <div className="grid grid-cols-2 w-full items-center gap-2 justify-center mt-4">
+                  <a
+                    href={`https://explorer.zksync.io/tx/${tx.data}`}
+                    target="_blank"
+                    className={cn(
+                      buttonVariants({ variant: 'outline' }),
+                      'rounded-full'
+                    )}
+                  >
+                    View on Block Explorer
+                  </a>
 
-          {transactions.map((transaction, index) => (
-            <TransactionGroup
-              key={index}
-              transaction={transaction}
-              onDelete={() =>
-                setTransactions((current) =>
-                  current.filter((_, i) => i !== index)
-                )
-              }
-              onChange={(update) =>
-                setTransactions((current) =>
-                  current.map((item, i) =>
-                    i === index ? { ...item, ...update } : item
-                  )
-                )
-              }
-            />
-          ))}
+                  <Link
+                    href={proposalId ? `/proposal/${proposalId}` : '/'}
+                    className={cn(
+                      buttonVariants({ variant: 'primary' }),
+                      'rounded-full'
+                    )}
+                  >
+                    {proposalId ? 'View Proposal' : 'Go Home'}
+                  </Link>
+                </div>
+              </>
+            )
+          }
 
-          <Button
-            type="submit"
-            variant={receipt.isError ? 'destructive' : 'primary'}
-            className="w-full rounded-full"
-            // Disable button if any transaction is missing data
-            disabled={
-              transactions.length === 0 ||
-              !hasEnoughVotingPower ||
-              transactions.some(
-                (transaction) =>
-                  !isAddress(transaction.target) || !isHex(transaction.calldata)
-              )
-            }
-            isLoading={tx.isPending || receipt.isLoading}
-          >
-            {tx.isPending
-              ? 'Pending Approval...'
-              : receipt.isError
-                ? 'Transaction Failed'
-                : receipt.isLoading
-                  ? 'Pending Transaction...'
-                  : 'Create Proposal'}
-          </Button>
-        </form>
+          return (
+            <>
+              {(!isConnected || !hasEnoughVotingPower) && (
+                <Alert variant="info" className="mb-4">
+                  <InfoIcon />
+                  <AlertTitle>View-only mode.</AlertTitle>
+                  <AlertDescription>
+                    Connect a wallet with &gt;21M votes to create a proposal.{' '}
+                    {votingPower !== undefined &&
+                      `You currently have ${bigintToFormattedString(votingPower)} votes.`}
+                  </AlertDescription>
+                </Alert>
+              )}
+
+              <form className="space-y-4" onSubmit={handleCreateProposal}>
+                <InputWrapper label="Governor Contract">
+                  <Select
+                    name="governorContract"
+                    defaultValue={ZkTokenGovernor.address}
+                  >
+                    <SelectTrigger className="w-full">
+                      <SelectValue placeholder="Select a category" />
+                    </SelectTrigger>
+                    <SelectContent>
+                      <SelectGroup>
+                        {Array.from(GOVERNORS.entries()).map(
+                          ([address, name]) => (
+                            <SelectItem key={address} value={address}>
+                              {name}
+                            </SelectItem>
+                          )
+                        )}
+                      </SelectGroup>
+                    </SelectContent>
+                  </Select>
+                </InputWrapper>
+
+                <InputWrapper label="Proposal Markdown">
+                  <Textarea placeholder="" required name="proposalMarkdown" />
+                </InputWrapper>
+
+                <div className="flex items-center gap-4 justify-between pt-4">
+                  <span>Transactions</span>
+                  <Button
+                    type="button"
+                    variant="outline"
+                    size="icon"
+                    className="h-8 w-8"
+                    onClick={() =>
+                      setTransactions([
+                        ...transactions,
+                        { target: '', value: 0, calldata: '' },
+                      ])
+                    }
+                  >
+                    <PlusIcon />
+                  </Button>
+                </div>
+
+                {transactions.map((transaction, index) => (
+                  <TransactionGroup
+                    key={index}
+                    transaction={transaction}
+                    onDelete={() =>
+                      setTransactions((current) =>
+                        current.filter((_, i) => i !== index)
+                      )
+                    }
+                    onChange={(update) =>
+                      setTransactions((current) =>
+                        current.map((item, i) =>
+                          i === index ? { ...item, ...update } : item
+                        )
+                      )
+                    }
+                  />
+                ))}
+
+                <Button
+                  type="submit"
+                  variant={receipt.isError ? 'destructive' : 'primary'}
+                  className="w-full rounded-full"
+                  // Disable button if any transaction is missing data
+                  disabled={
+                    transactions.length === 0 ||
+                    !hasEnoughVotingPower ||
+                    transactions.some(
+                      (transaction) =>
+                        !isAddress(transaction.target) ||
+                        !isHex(transaction.calldata)
+                    )
+                  }
+                  isLoading={tx.isPending || receipt.isLoading}
+                >
+                  {tx.isPending
+                    ? 'Pending Approval...'
+                    : receipt.isError
+                      ? 'Transaction Failed'
+                      : receipt.isLoading
+                        ? 'Pending Transaction...'
+                        : 'Create Proposal'}
+                </Button>
+              </form>
+            </>
+          )
+        })()}
       </main>
     </div>
   )
